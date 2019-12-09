eaa1473417f526a152338effafab3b135f56c623
davidnguyen179 <dzungnguyen179@gmail.com>
2019-12-09 21:15:56 +0900

    Solve sentence-similarity with Set

--- leetcode/sentence-similarity/index.js
+++ leetcode/sentence-similarity/index.js
@@ -5,38 +5,23 @@
  * @param {string[][]} pairs
  * @return {boolean}
  */
+// Using Set
 var areSentencesSimilar = function(words1, words2, pairs) {
   if (words1.length !== words2.length) return false;
 
-  if (pairs.length === 0) {
-    for (let i = 0; i < words1.length; i++) {
-      if (words1[i] !== words2[i]) {
-        return false;
-      }
-    }
-    return true;
+  var pairsSet = new Set();
+  for (let i = 0; i < pairs.length; i++) {
+    pairsSet.add(`${pairs[i][0]}-${pairs[i][1]}`);
   }
 
   let count = 0;
-
   for (let i = 0; i < words1.length; i++) {
     const w1 = words1[i];
     const w2 = words2[i];
     if (w1 === w2) {
       count++;
-    } else {
-      for (let j = 0; j < pairs.length; j++) {
-        const item1 = pairs[j][0];
-        const item2 = pairs[j][1];
-  
-        if (
-          (item1 === w1 && item2 === w2) ||
-          (item2 === w1 && item1 === w2)
-        ) {
-          count++;
-          break;
-        }
-      }
+    } else if(pairsSet.has(`${w1}-${w2}`) || pairsSet.has(`${w2}-${w1}`)) {
+      count++;
     }
   }
 
@@ -46,18 +31,60 @@ var areSentencesSimilar = function(words1, words2, pairs) {
   return false;
 };
 
-// var words1 = ['great', 'great', 'acting', 'fine']; // true
-// var words2 = ['fine', 'cool', 'drama', 'great'];
-// var pairs = [
-//   ['great', 'fine'],
-//   ['great', 'cool'],
-//   ['fine', 'great'],
-//   ['acting','drama']
-// ];
+// Without using Set
+// var areSentencesSimilar = function(words1, words2, pairs) {
+//   if (words1.length !== words2.length) return false;
 
-// var words1 = ["great", "acting", "skills"]; // false
-// var words2 = ["fine", "drama", "talent"];
-// var pairs = [["great", "good"], ["fine", "good"], ["acting","drama"], ["skills","talent"]];
+//   if (pairs.length === 0) {
+//     for (let i = 0; i < words1.length; i++) {
+//       if (words1[i] !== words2[i]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+
+//   let count = 0;
+
+//   for (let i = 0; i < words1.length; i++) {
+//     const w1 = words1[i];
+//     const w2 = words2[i];
+//     if (w1 === w2) {
+//       count++;
+//     } else {
+//       for (let j = 0; j < pairs.length; j++) {
+//         const item1 = pairs[j][0];
+//         const item2 = pairs[j][1];
+  
+//         if (
+//           (item1 === w1 && item2 === w2) ||
+//           (item2 === w1 && item1 === w2)
+//         ) {
+//           count++;
+//           break;
+//         }
+//       }
+//     }
+//   }
+
+//   if (count === words1.length) {
+//     return true;
+//   }
+//   return false;
+// };
+
+var words1 = ['great', 'great', 'acting', 'fine']; // true
+var words2 = ['fine', 'cool', 'drama', 'great'];
+var pairs = [
+  ['great', 'fine'],
+  ['great', 'cool'],
+  ['fine', 'great'],
+  ['acting','drama']
+];
+
+var words1 = ["great", "acting", "skills"]; // false
+var words2 = ["fine", "drama", "talent"];
+var pairs = [["great", "good"], ["fine", "good"], ["acting","drama"], ["skills","talent"]];
 
 // var words1 = ["great"]; // true
 // var words2 = ["great"];
@@ -79,8 +106,8 @@ var areSentencesSimilar = function(words1, words2, pairs) {
 // var words2 = ["fine","drama","talent"]
 // var pairs = [["great","fine"],["drama","acting"],["skills","talent"]]
 
-var words1 = ["great","acting","skills", "one"] // true
-var words2 = ["fine","drama","talent", "one"]
-var pairs = [["great","fine"],["drama","acting"],["skills","talent"]]
+// var words1 = ["great","acting","skills", "one"] // true
+// var words2 = ["fine","drama","talent", "one"]
+// var pairs = [["great","fine"],["drama","acting"],["skills","talent"]]
 
 console.log(areSentencesSimilar(words1, words2, pairs));
