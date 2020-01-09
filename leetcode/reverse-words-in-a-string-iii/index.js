397aed6de7afb87b5146c97cf90d0256140d22a3
davidnguyen179 <dzungnguyen179@gmail.com>
2020-01-09 11:52:20 +0900

    Solve reverse-words-in-a-string-iii with O(n)

--- leetcode/reverse-words-in-a-string-iii/index.js
+++ leetcode/reverse-words-in-a-string-iii/index.js
@@ -3,25 +3,46 @@
  * @param {string} s
  * @return {string}
  */
+// var reverseWords = function(s) {
+//   if (!s || s.length === 0) return s;
+
+//   var arrS = s.split(' ');
+//   var length = arrS.length;
+//   var result = '';
+//   var wordStr = '';
+//   var wordLength;
+//   var word;
+
+//   for (var i = 0; i < length; i++) {
+//     word = arrS[i];
+//     wordLength = word.length;
+
+//     for (var j = 0; j < wordLength; j++) {
+//       wordStr = wordStr + word[wordLength - 1 - j];
+//     }
+//     result = result + wordStr + ' ';
+//     wordStr = '';
+//   }
+
+//   return result.trim();
+// };
+
 var reverseWords = function(s) {
   if (!s || s.length === 0) return s;
 
-  var arrS = s.split(' ');
-  var length = arrS.length;
-  var result = '';
-  var wordStr = '';
-  var wordLength;
-  var word;
+  const strLength = s.length;
+  let reversedStr = '';
 
-  for (var i = 0; i < length; i++) {
-    word = arrS[i];
-    wordLength = word.length;
+  for (var i = 0; i < strLength; i++) {
+    reversedStr = reversedStr + s[strLength - 1 - i];
+  }
 
-    for (var j = 0; j < wordLength; j++) {
-      wordStr = wordStr + word[wordLength - 1 - j];
-    }
-    result = result + wordStr + ' ';
-    wordStr = '';
+  const arr = reversedStr.split(' ');
+  const arrLength = arr.length;
+  let result = '';
+
+  for (var i = 0; i < arrLength; i++) {
+    result = result + arr[arrLength - 1 - i] + ' ';
   }
 
   return result.trim();
