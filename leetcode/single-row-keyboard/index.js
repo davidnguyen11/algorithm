7cfddd76467d86d1b069d6e5cd24f455f7fa4a50
davidnguyen179 <dzungnguyen179@gmail.com>
2020-03-03 16:29:20 +0900

    Solve single-row-keyboard

---
+++ leetcode/single-row-keyboard/index.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string} keyboard
+ * @param {string} word
+ * @return {number}
+ */
+var calculateTime = function(keyboard, word) {
+  var mapKeyboard = {};
+  var KEYBOARD_LENGTH = 26;
+
+  for (var i = 0; i < KEYBOARD_LENGTH; i++) {
+    mapKeyboard[keyboard[i]] = i;
+  }
+
+  if (word.length === 1) {
+    return mapKeyboard[word[0]];
+  }
+
+  var startFrom = 0;
+  var result = [];
+  var keyIndex;
+  for (var i = 0; i < word.length; i++) {
+    keyIndex = mapKeyboard[word[i]];
+    result.push(Math.abs(startFrom - keyIndex));
+    startFrom = keyIndex;
+  }
+
+  var sum = 0;
+  for (var i = 0; i < result.length; i++) {
+    sum += result[i];
+  }
+  return sum;
+};
+
+var keyboard = 'abcdefghijklmnopqrstuvwxyz';
+var word = 'cba';
+
+var keyboard = 'pqrstuvwxyzabcdefghijklmno';
+var word = 'leetcode';
+
+var keyboard = 'pqrstuvwxyzabcdefghijklmno';
+var word = 'n';
+
+console.log(calculateTime(keyboard, word));
