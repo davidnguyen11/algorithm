b4f324a25ddd48b367ae7459154ce6dec26a2da1
davidnguyen179 <dzungnguyen179@gmail.com>
2020-03-03 23:36:17 +0900

    Add tail recursion single-row-keyboard

--- leetcode/single-row-keyboard/index.js
+++ leetcode/single-row-keyboard/index.js
@@ -3,6 +3,36 @@
  * @param {string} word
  * @return {number}
  */
+// var calculateTime = function(keyboard, word) {
+//   var mapKeyboard = {};
+//   var KEYBOARD_LENGTH = 26;
+
+//   for (var i = 0; i < KEYBOARD_LENGTH; i++) {
+//     mapKeyboard[keyboard[i]] = i;
+//   }
+
+//   if (word.length === 1) {
+//     return mapKeyboard[word[0]];
+//   }
+
+//   var startFrom = 0;
+//   var result = [];
+//   var keyIndex;
+//   for (var i = 0; i < word.length; i++) {
+//     keyIndex = mapKeyboard[word[i]];
+//     result.push(Math.abs(startFrom - keyIndex));
+//     startFrom = keyIndex;
+//   }
+
+//   var sum = 0;
+//   for (var i = 0; i < result.length; i++) {
+//     sum += result[i];
+//   }
+//   return sum;
+// };
+
+// Solution 2
+// using tail recursion
 var calculateTime = function(keyboard, word) {
   var mapKeyboard = {};
   var KEYBOARD_LENGTH = 26;
@@ -10,26 +40,15 @@ var calculateTime = function(keyboard, word) {
   for (var i = 0; i < KEYBOARD_LENGTH; i++) {
     mapKeyboard[keyboard[i]] = i;
   }
-
-  if (word.length === 1) {
-    return mapKeyboard[word[0]];
-  }
-
-  var startFrom = 0;
-  var result = [];
-  var keyIndex;
-  for (var i = 0; i < word.length; i++) {
-    keyIndex = mapKeyboard[word[i]];
-    result.push(Math.abs(startFrom - keyIndex));
-    startFrom = keyIndex;
-  }
-
-  var sum = 0;
-  for (var i = 0; i < result.length; i++) {
-    sum += result[i];
-  }
-  return sum;
+  return helper(mapKeyboard, word, 0, 0, 0);
 };
+
+function helper(mapKeyboard, word, currentIndex, startFrom, sum) {
+  if (currentIndex >= word.length) return sum;
+  var keyIndex = mapKeyboard[word[currentIndex]];
+  var value = Math.abs(startFrom - keyIndex);
+  return helper(mapKeyboard, word, currentIndex + 1, keyIndex, sum + value);
+}
 
 var keyboard = 'abcdefghijklmnopqrstuvwxyz';
 var word = 'cba';
