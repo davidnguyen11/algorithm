c6c777464af5b863b4eb0a92ef31bdbcccb9f731
davidnguyen179 <dzungnguyen179@gmail.com>
2020-03-09 14:35:58 +0900

    Solve sum-of-left-leaves

---
+++ leetcode/sum-of-left-leaves/index.js
@@ -0,0 +1,79 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function(node) {
+  if (!node) return 0;
+
+  var sum = 0;
+  if (node.left && !node.left.left && !node.left.right) {
+    sum += node.left.val;
+  }
+
+  return sum + sumOfLeftLeaves(node.left) + sumOfLeftLeaves(node.right);
+};
+
+var node = {
+  val: 3,
+  left: {
+    val: 9,
+  },
+  right: {
+    val: 20,
+    left: {
+      val: 15,
+    },
+    right: {
+      val: 7,
+    },
+  },
+};
+
+var node = {
+  val: 3,
+};
+
+var node = {
+  val: 3,
+  left: {
+    val: 4,
+    left: {
+      val: 5,
+    },
+  },
+};
+
+var node = {
+  val: 3,
+  right: {
+    val: 4,
+    right: {
+      val: 5,
+    },
+  },
+};
+
+var node = {
+  val: 3,
+  left: {
+    val: 4,
+    left: {
+      val: 5,
+    },
+  },
+  right: {
+    val: 10,
+    left: {
+      val: 1,
+    },
+  },
+};
+
+console.log(sumOfLeftLeaves(node));
