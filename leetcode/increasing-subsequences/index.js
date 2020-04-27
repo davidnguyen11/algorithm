86c9156d7d3b689890e3e41d954a07d3161f863a
davidnguyen179 <dzungnguyen179@gmail.com>
2020-04-27 17:52:59 +0900

    Solve increasing-subsequences

--- leetcode/increasing-subsequences/index.js
+++ leetcode/increasing-subsequences/index.js
@@ -1,16 +1,34 @@
 var findSubsequences = function (nums) {
-  nums.sort(function (a, b) { return a - b });
   var result = [];
-
   helper(nums, 0, [], result);
   return result;
 };
 
-function helper(nums, i, temp, result) {
+function helper(nums, index, curr, ans) {
+  if (curr.length >= 2) {
+    ans.push([...curr]);
+  }
 
+  var visited = new Set();
+  for (var i = index; i < nums.length; i++) {
+    if (visited.has(nums[i])) {
+      continue;
+    }
 
+    if (curr.length === 0 || curr[curr.length - 1] <= nums[i]) {
+      visited.add(nums[i]);
+      curr.push(nums[i]);
+
+      helper(nums, i + 1, curr, ans);
+
+      curr.pop();
+    }
+  }
 }
 
 var nums = [4, 6, 7, 7];
-console.log(findSubsequences(nums));
+// var nums = [5, 1, 6, 9];
+// var nums = [1, 1, 1, 1];
 
+// var nums = [4, 3, 2, 1];
+console.log('result', findSubsequences(nums));
