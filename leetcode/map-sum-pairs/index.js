68178e4047ea45b7d58a624f00015c688ce84411
davidnguyen179 <dzungnguyen179@gmail.com>
2020-12-14 18:21:26 +0900

    Solve map-sum-pairs with Trie approach

--- leetcode/map-sum-pairs/index.js
+++ leetcode/map-sum-pairs/index.js
@@ -5,8 +5,9 @@
 var MapSum = function () {
   // store all keywords in array to search prefix using startWith function of JS
   // hash map to store key and weight
-  this.keywords = []; // another way is to do that is using Trie to build the keywords tree
+  // this.keywords = []; // another way is to do that is using Trie to build the keywords tree
   this.table = {};
+  this.trie = new Trie();
 };
 
 /**
@@ -15,9 +16,7 @@ var MapSum = function () {
  * @return {void}
  */
 MapSum.prototype.insert = function (key, val) {
-  if (!this.table[key]) {
-    this.keywords.push(key);
-  }
+  this.trie.insert(key);
   this.table[key] = val;
 };
 
@@ -26,23 +25,70 @@ MapSum.prototype.insert = function (key, val) {
  * @return {number}
  */
 MapSum.prototype.sum = function (prefix) {
-  const listKeys = [];
-  for (let i = 0; i < this.keywords.length; i++) {
-    if (this.keywords[i].startsWith(prefix)) {
-      listKeys.push(this.keywords[i]);
-    }
-  }
+  // const listKeys = [];
+  // for (let i = 0; i < this.keywords.length; i++) {
+  //   if (this.keywords[i].startsWith(prefix)) {
+  //     listKeys.push(this.keywords[i]);
+  //   }
+  // }
+
+  const listKeys = this.trie.searchByPrefix(prefix);
 
   let sum = 0;
   for (let i = 0; i < listKeys.length; i++) {
-    sum += this.table[listKeys[i]];
+    if (this.table[listKeys[i]]) {
+      sum += this.table[listKeys[i]];
+    }
   }
   return sum;
 };
 
-/**
- * Your MapSum object will be instantiated and called as such:
- * var obj = new MapSum()
- * obj.insert(key,val)
- * var param_2 = obj.sum(prefix)
- */
+function Trie() {
+  this.children = {};
+  this.isEndWord = false;
+}
+
+Trie.prototype.insert = function (word) {
+  let self = this;
+  for (const item of word) {
+    if (!self.children[item]) {
+      self.children[item] = new Trie();
+    }
+    self = self.children[item];
+  }
+  self.isEndWord = true;
+}
+
+Trie.prototype.searchByPrefix = function (prefix) {
+  let self = this;
+
+  for (const c of prefix) {
+    if (self.children[c]) {
+      self = self.children[c];
+    }
+  }
+
+  const ans = [];
+  self.getAutoComplete(prefix, ans);
+  return ans;
+}
+
+Trie.prototype.getAutoComplete = function (c, ans) {
+  var self = this;
+  if (self.isEndWord) {
+    ans.push(c);
+  }
+
+  for (var key in self.children) {
+    if (self.children[key]) {
+      self.children[key].getAutoComplete(c + key, ans);
+    }
+  }
+}
+
+var t = new Trie();
+t.insert('app');
+t.insert('apple')
+t.searchByPrefix('app');
+
+console.log(t.children);
