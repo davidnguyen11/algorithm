d241176f39f12b40ae05618092044b0528960fba
davidnguyen179 <dzungnguyen179@gmail.com>
2021-01-15 09:46:37 +0900

    Add 3 pointers approach for  intersection-of-three-sorted-arrays

--- leetcode/intersection-of-three-sorted-arrays/index.js
+++ leetcode/intersection-of-three-sorted-arrays/index.js
@@ -7,27 +7,53 @@
  */
 var arraysIntersection = function (arr1, arr2, arr3) {
   const result = [];
+  let p1 = 0;
+  let p2 = 0;
+  let p3 = 0;
 
-  for (let i = 0; i < arr1.length; i++) {
-    const index = search(arr2, 0, arr2.length, arr1[i]);
-    const index2 = search(arr3, 0, arr3.length, arr2[index]);
-
-    if (index2 !== -1) {
-      result.push(arr2[index]);
+  while (p1 < arr1.length && p2 < arr2.length && p3 < arr3.length) {
+    if (arr1[p1] === arr2[p2] && arr2[p2] === arr3[p3]) {
+      result.push(arr1[p1]);
+      p1++;
+      p2++;
+      p3++;
+    } else {
+      if (arr1[p1] < arr2[p2]) {
+        p1++;
+      } else if (arr2[p2] < arr3[p3]) {
+        p2++;
+      } else {
+        p3++;
+      }
     }
   }
 
   return result;
 };
 
-function search(arr, l, r, target) {
-  if (l > r) return -1;
+// var arraysIntersection = function (arr1, arr2, arr3) {
+//   const result = [];
 
-  const mid = (l + (r - l) / 2) >> 0;
+//   for (let i = 0; i < arr1.length; i++) {
+//     const index = search(arr2, 0, arr2.length, arr1[i]);
+//     const index2 = search(arr3, 0, arr3.length, arr2[index]);
 
-  if (arr[mid] === target) return mid;
+//     if (index2 !== -1) {
+//       result.push(arr2[index]);
+//     }
+//   }
 
-  if (arr[mid] < target)
-    return search(arr, mid + 1, r, target);
-  return search(arr, l, mid - 1, target);
-}
+//   return result;
+// };
+
+// function search(arr, l, r, target) {
+//   if (l > r) return -1;
+
+//   const mid = (l + (r - l) / 2) >> 0;
+
+//   if (arr[mid] === target) return mid;
+
+//   if (arr[mid] < target)
+//     return search(arr, mid + 1, r, target);
+//   return search(arr, l, mid - 1, target);
+// }
