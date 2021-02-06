6a0e848ccce4e3e5294da20803997e1b9e2a1d66
davidnguyen179 <dzungnguyen179@gmail.com>
2021-02-06 22:10:00 +0900

    Add hash table approach

--- leetcode/contains-duplicate-ii/index.js
+++ leetcode/contains-duplicate-ii/index.js
@@ -4,34 +4,25 @@
  * @param {number} k
  * @return {boolean}
  */
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+// Hash table
 var containsNearbyDuplicate = function (nums, k) {
-  const newNums = [];
+  const map = {};
 
   for (let i = 0; i < nums.length; i++) {
-    newNums.push({ index: i, val: nums[i] });
-  }
+    const curr = nums[i];
 
-  newNums.sort((a, b) => a.val - b.val);
-
-  for (let i = 0; i < newNums.length; i++) {
-    const pos = search(newNums, i + 1, newNums.length - 1, newNums[i]);
-
-    if (pos !== -1 && Math.abs(newNums[pos].index - newNums[i].index) <= k) {
-      return true;
+    if (map[curr] !== undefined) {
+      if (Math.abs(i - map[curr]) <= k) {
+        return true;
+      }
     }
+    map[curr] = i;
   }
 
   return false;
 };
-
-function search(nums, left, right, target) {
-  if (left <= right) {
-    const mid = Math.floor(left + (right - left) / 2);
-
-    if (nums[mid].val === target.val) return mid;
-
-    if (nums[mid].val < target) return search(nums, mid + 1, right, target);
-    return search(nums, left, mid - 1, target);
-  }
-  return -1;
-}
