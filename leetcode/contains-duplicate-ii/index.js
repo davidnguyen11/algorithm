839c0437c01083a639203f68ac3e0a4e3224deb0
davidnguyen179 <dzungnguyen179@gmail.com>
2021-02-06 21:44:19 +0900

    Solve contains-duplicate-ii

---
+++ leetcode/contains-duplicate-ii/index.js
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/contains-duplicate-ii
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function (nums, k) {
+  const newNums = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    newNums.push({ index: i, val: nums[i] });
+  }
+
+  newNums.sort((a, b) => a.val - b.val);
+
+  for (let i = 0; i < newNums.length; i++) {
+    const pos = search(newNums, i + 1, newNums.length - 1, newNums[i]);
+
+    if (pos !== -1 && Math.abs(newNums[pos].index - newNums[i].index) <= k) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+function search(nums, left, right, target) {
+  if (left <= right) {
+    const mid = Math.floor(left + (right - left) / 2);
+
+    if (nums[mid].val === target.val) return mid;
+
+    if (nums[mid].val < target) return search(nums, mid + 1, right, target);
+    return search(nums, left, mid - 1, target);
+  }
+  return -1;
+}
